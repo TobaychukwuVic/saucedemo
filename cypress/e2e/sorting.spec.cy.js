dc92d1bb13e61b156e176bac717cac067ad28f9b
tukoha <ukohatobay@gmail.com>
2024-08-16 17:04:01 +0100

    First Commit5

--- cypress/e2e/sorting.spec.cy.js
+++ cypress/e2e/sorting.spec.cy.js
@@ -25,21 +25,4 @@ describe('Sauce Demo Sort Test', () => {
     });
   });
 
-  //it('Change sorting to Name (Z -> A) and verify', () => {
-    // Login to the site
-//    cy.get('#user-name').type('standard_user');
-//    cy.get('#password').type('secret_sauce');
-//    cy.get('#login-button').click();
 
-//    // Change the sorting to Name (Z -> A)
-//    cy.get('.product_sort_container').select('Name (Z to A)');
-//
-//    // Verify sorting by Name (Z -> A)
-//    cy.get('.inventory_item_name').then(items => {
-//      let itemNames = [...items].map(item => item.innerText);
-//      let sortedNames = [...itemNames].sort((a, b) => b.localeCompare(a));
-//      expect(itemNames).to.deep.equal(sortedNames);
-//    });
-//  });
-
-//});
